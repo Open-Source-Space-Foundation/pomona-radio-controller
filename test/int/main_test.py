e41018776d8e9f6f211f5bec054f3a0c4b800163
Jack Pearson <jack@pearson.onl>
2026-02-07 09:31:53 -0800

    add failing test for second serial port

--- test/int/main_test.py
+++ test/int/main_test.py
@@ -1,3 +1,8 @@
 def test_send_noop(fprime_test_api):
     fprime_test_api.send_and_assert_command("CdhCore.cmdDisp.CMD_NO_OP", max_delay=0.1)
     assert fprime_test_api.get_command_test_history().size() == 1
+
+
+def test_open_data_port():
+    tty = open("/dev/serial/by-id/usb-F_Prime_Pomona_Ground_Station-if02")
+    tty.close()
